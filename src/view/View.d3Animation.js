ffd11075707a6048491113d0cd2fb72a42b97921
donatso <soricdonat@gmail.com>
2021-11-26 17:20:29 -0500

    checkpoint - animation tweaks

--- src/view/View.d3Animation.js
+++ src/view/View.d3Animation.js
@@ -107,7 +107,7 @@ export default function d3AnimationView({store, cont, Card}) {
         ancestry_levels = Math.max(...tree.data.map(d=>d.is_ancestry ? d.depth : 0))
       let delay = d.depth*delay_level;
       if ((d.depth !== 0 || !!d.spouse) && !d.is_ancestry) {
-        delay+=(ancestry_levels+1)*delay_level  // after ancestry
+        delay+=(ancestry_levels)*delay_level  // after ancestry
         if (d.spouse) delay+=delay_level  // spouse after bloodline
         delay+=(d.depth)*delay_level  // double the delay for each level because of additional spouse delay
       }
