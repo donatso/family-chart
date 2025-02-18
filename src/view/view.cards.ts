c72e21a1f9d8db86d760efe4c42463db15cf09b5
simonljus <simon.ljus94@gmail.com>
2025-02-18 13:09:50 +0100

    to ts

--- src/view/view.cards.ts
+++ src/view/view.cards.ts
@@ -2,11 +2,11 @@ import * as d3 from 'd3';
 import {calculateEnterAndExitPositions} from "../CalculateTree/CalculateTree.handlers.js"
 import {calculateDelay} from "./view.utils.ts"
 
-export default function updateCards(svg, tree, Card, props={}) {
-  const card = d3.select(svg).select(".cards_view").selectAll("g.card_cont").data(tree.data, d => d.data.id),
+export default function updateCards(svg, tree, Card, props: {initial?: boolean,transition_time?: number,}={}) {
+  const card = d3.select(svg).select(".cards_view").selectAll("g.card_cont").data(tree.data, (d: any) => d.data.id),
     card_exit = card.exit(),
     card_enter = card.enter().append("g").attr("class", "card_cont"),
-    card_update = card_enter.merge(card)
+    card_update = card_enter.merge(card as any )
 
   card_exit.each(d => calculateEnterAndExitPositions(d, false, true))
   card_enter.each(d => calculateEnterAndExitPositions(d, true, false))
@@ -29,12 +29,12 @@ export default function updateCards(svg, tree, Card, props={}) {
   function cardUpdate(d) {
     Card.call(this, d)
     const delay = props.initial ? calculateDelay(tree, d, props.transition_time) : 0;
-    d3.select(this).transition().duration(props.transition_time).delay(delay).attr("transform", `translate(${d.x}, ${d.y})`).style("opacity", 1)
+    d3.select(this).transition().duration(props.transition_time!).delay(delay).attr("transform", `translate(${d.x}, ${d.y})`).style("opacity", 1)
   }
 
   function cardExit(d) {
     const g = d3.select(this)
-    g.transition().duration(props.transition_time).style("opacity", 0).attr("transform", `translate(${d._x}, ${d._y})`)
+    g.transition().duration(props.transition_time!).style("opacity", 0).attr("transform", `translate(${d._x}, ${d._y})`)
       .on("end", () => g.remove())
   }
 }
