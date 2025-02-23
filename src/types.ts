2f8e5b9e82f760252b2cbb2084579c22990e17e1
simonljus <simon.ljus94@gmail.com>
2025-02-23 10:57:59 +0100

    specify type

--- src/types.ts
+++ src/types.ts
@@ -12,12 +12,12 @@ export type TreePerson = {
         avatar:string
         gender: 'M'|'F' | string
     }> & Partial<Record<string,string>>
-    _new_rel_data?: {rel_type:string,label:string, other_parent_id?: unknown}
+    _new_rel_data?: {rel_type:string,label:string, other_parent_id?: string}
     main?:boolean
     to_add?:boolean
-    hide_rels?:unknown
+    hide_rels?:boolean
 }
-export type FamilyTreeNodePerson = {parents?: FamilyTreeNode[], spouses: FamilyTreeNode[], spouse: FamilyTreeNode,depth: unknown,is_ancestry?:boolean,x: number, y: number,added?:unknown,all_rels_displayed?:boolean,_x?: number, _y?: number, sx?: number, psx?: number,psy?: number, exiting: unknown}
+export type FamilyTreeNodePerson = {parents?: FamilyTreeNode[], spouses: FamilyTreeNode[], spouse: FamilyTreeNode,depth: number,is_ancestry?:boolean,x: number, y: number,added?:boolean,all_rels_displayed?:boolean,_x?: number, _y?: number, sx?: number, psx?: number,psy?: number, exiting: unknown}
 export type HierarchyLikePerson= {data: TreePerson, depth: number} & Pretty
 export type HierarchyPerson = HierarchyNode<TreePerson> | {data: TreePerson, depth: number}
 type Pretty = {is_ancestry?: boolean, added?:unknown, x?: number, y?: number, sx?: number, sy?: number, psx?: number | undefined, psy?: number |undefined,spouse?: HierarchyPersonPretty,spouses?: HierarchyPersonPretty[]}
