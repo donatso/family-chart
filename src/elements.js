fa1665a072d65cb71b2e2320e635785b15cbea85
donatso <donat@clearbluetechnologies.com>
2025-07-19 09:42:45 -0300

    elements export fix

--- src/elements.js
+++ src/elements.js
@@ -1,4 +1,4 @@
 export * from './renderers/card-svg/elements'
-export * from './renderers/card-html'
-export * from './renderers/card-svg/card-svg'
+export {default as CardHtml} from './renderers/card-html'
+export {default as CardSvg} from './renderers/card-svg/card-svg'
 export {default as infoPopup} from './features/info-popup'
